77e857151f098d5babda2ce9a967499c411a9451
Fatih Kaya <1994274@gmail.com>
2021-04-26 12:17:48 +0300

    use typescript

---
+++ index.ts
@@ -0,0 +1,56 @@
+import * as fs from 'fs'
+import * as cheerio from 'cheerio'
+const config = require('./config/desktop.json');
+const { collection, blockSelector } = config;
+
+async function main() {
+    const data = fs.readFileSync('./sample/desktop.html', { encoding: 'utf-8' })
+    console.time("parser")
+    const $ = cheerio.load(data);
+    const blocks = $(blockSelector);
+    const results = [];
+    const typeOrders = {};
+
+    blocks.each((index, el) => {
+        Object.keys(collection).forEach(key => {
+            const currentType = collection[key];
+            const typeCheck = $(el).find(currentType.detect.withInnerSelector).length > 0;
+
+            if (!typeCheck) return;
+
+            const schema = currentType.schema;
+            const result = {};
+
+            typeOrders[key] = (typeOrders[key] || 0) + 1;
+
+            Object.keys(schema).forEach(field => {
+                const isObjectSelector = typeof schema[field] === 'object';
+                let selector = schema[field];
+                let collectType = "text";           // available: "text" | "html" | "attr" 
+                let collectParams = undefined;
+
+                if (isObjectSelector) {
+                    selector = schema[field].selector;
+                    collectType = schema[field].attr ? "attr" : "text";
+                    collectParams =  schema[field].attr;
+                }
+
+                result[field] = $(el).find(selector)[collectType](collectParams);
+            });
+
+            results.push({
+                order: index,
+                typeOrder: typeOrders[key],
+                type: key,
+                ...result,
+            })
+        })
+    });
+
+    console.log(results);
+    console.timeEnd('parser');
+}
+
+main().catch(err => {
+    console.log('Error:', err)
+})
