02fca824e3c63816dbcb9b9bf8f991be353168cd
Aykut Kardaş <aykutkrds@gmail.com>
2021-05-10 10:32:38 +0300

    Add utils tests

---
+++ src/utils/utils.spec.ts
@@ -0,0 +1,28 @@
+import { expect } from 'chai';
+import 'mocha';
+
+import execRegex from './execRegex';
+import transformValueType from './transformValueType';
+
+describe('Utils Tests', () => {
+  it('execRegex Method', () => {
+    const regex = { pattern: '\\d+', flags: 'g' };
+    const value = execRegex('Lorem Ipsum 21 Temp', regex);
+    expect('21').to.deep.equal(value);
+  });
+
+  it('transformValueType Method - Number', () => {
+    const value = transformValueType('21', 'number');
+    expect(21).to.deep.equal(value);
+  });
+
+  it('transformValueType Method - Boolean', () => {
+    const value = transformValueType('lorem', 'boolean');
+    expect(true).to.deep.equal(value);
+  });
+
+  it('transformValueType Method - Boolean', () => {
+    const value = transformValueType('', 'boolean');
+    expect(false).to.deep.equal(value);
+  });
+});
