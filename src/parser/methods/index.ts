6412ff6b373e1680d8f5b4b6f8682f0a4538a154
Aykut Kardaş <aykutkrds@gmail.com>
2021-05-22 14:13:02 +0300

    Add trim pipeline method

--- src/parser/methods/index.ts
+++ src/parser/methods/index.ts
@@ -1,9 +1,11 @@
 import toFloat from './float';
 import toNumber from './number';
 import toBoolean from './boolean';
+import toTrim from './trim';
 
 export default {
   float: toFloat,
   number: toNumber,
-  boolean: toBoolean
+  boolean: toBoolean,
+  trim: toTrim
 };
