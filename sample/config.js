3b4c81c64f235b31215aee1b2e24f54c81de1d10
Aykut Kardaş <aykutkrds@gmail.com>
2021-05-27 12:45:10 +0300

    Update sample

--- sample/config.js
+++ sample/config.js
@@ -1,34 +1,12 @@
 const Config = {
-  result: {
-    selector: '.uEierd, .hlcw0c .g, #rso > div > div',
-    type: 'array',
-    schema: (el) => {
-      if (el.hasClass('uEierd')) {
-        return {
-          title: '.c4Djg',
-          link: {
-            schema: {
-              url: '.d5oMvf > a @ href',
-              title: '.qzEoUe'
-            }
-          },
-          phoneNumber: '.NVWord + .WZ8Tjf',
-          siteLinks: {
-            selector: '.fCBnFe | array',
-            schema: {
-              url: '.aLF0Z > a @ href',
-              description: '.yDYNvb.aLF0Z'
-            }
-          },
-          address: '.Qezod :nth-child(2)',
-          availableHours: '.Qezod :nth-child(3)',
-          shoppingRating: {
-            selector: '.aLF0Z div:nth-child(2)'
-          },
-          leadForm: '.Qc4Zr'
-        };
-      } else {
-        return {};
+  selector: '.uEierd',
+  type: 'array',
+  schema: {
+    title: '.c4Djg',
+    link: {
+      schema: {
+        url: '.d5oMvf > a @ href',
+        name: '.qzEoUe'
       }
     }
   }
--- sample/index.js
+++ sample/index.js
@@ -1,26 +1,21 @@
 const fs = require('fs');
 const config = require('./config');
 
-const { parse, validateConfig } = require('../build/index');
+const { parse } = require('../build/index');
 
 async function main() {
   const data = fs.readFileSync(__dirname + '/desktop.html', {
     encoding: 'utf-8'
   });
 
-  const errors = validateConfig(config);
-
-  console.log('errors', errors);
   console.time('parser');
-
-  const results = {};
-
-  Object.keys(config).forEach((key) => {
-    results[key] = parse(config[key], data);
-  });
-
-  console.log(JSON.stringify(results, null, 2));
+  const results = parse(data, config);
   console.timeEnd('parser');
+
+  fs.writeFileSync(
+    __dirname + '/parse-sample.json',
+    JSON.stringify(results, null, 2)
+  );
 }
 
 main().catch((err) => {
