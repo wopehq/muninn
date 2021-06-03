05d0e1246eb9689656c7a516087f1ab28c490b99
Aykut Kardaş <aykutkrds@gmail.com>
2021-06-03 10:43:59 +0300

    Fix sample script

--- sample/index.js
+++ sample/index.js
@@ -4,10 +4,14 @@ const config = require('./config');
 
 const { parse } = require('../build/index');
 
-const { data } = await axios.get(
-  'https://www.amazon.com/AMD-Ryzen-3700X-16-Thread-Processor/dp/B07SXMZLPK/'
-);
+async function main() {
+  const { data } = await axios.get(
+    'https://www.amazon.com/AMD-Ryzen-3700X-16-Thread-Processor/dp/B07SXMZLPK/'
+  );
 
-const result = parse(data, config);
+  const result = parse(data, config);
 
-fs.writeFileSync(__dirname + '/output.json', JSON.stringify(result, null, 2));
+  fs.writeFileSync(__dirname + '/output.json', JSON.stringify(result, null, 2));
+}
+
+main();
