4f3a80d981cf1352fb9dbad9fb7678bce25fe73c
Aykut Kardaş <aykutkrds@gmail.com>
2021-06-04 10:03:57 +0300

    Add `size` feature

--- src/parser/getArrayValue.ts
+++ src/parser/getArrayValue.ts
@@ -5,6 +5,10 @@ import getValue from './getValue';
 function getArrayValue({ $, el: element }: ElementPassArg, config) {
   const values = [];
 
+  if (config?.methods?.includes('size')) {
+    return $(element).length;
+  }
+
   $(element).each((index, el) => {
     const { selector, type, ...rest } = config;
     const value = getValue({ $, el: $(el) }, rest);
