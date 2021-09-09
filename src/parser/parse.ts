9a09ffebf169ef7d602aa160366f0c155c950d18
Aykut Kardaş <aykutkrds@gmail.com>
2021-09-09 09:05:02 +0300

    Add cheerio.Root support to data argument of parse

--- src/parser/parse.ts
+++ src/parser/parse.ts
@@ -1,9 +1,15 @@
-import { load } from 'cheerio';
+import { load, Root } from 'cheerio';
 import { InputConfig } from '../config/types';
 import getValue from './getValue';
 
-function parse(data: string, config: InputConfig): Object {
-  const $ = load(data);
+function parse(data: string | Root, config: InputConfig): Object {
+  let $;
+
+  if (typeof data === 'string') {
+    $ = load(data);
+  } else if (typeof data === 'function') {
+    $ = data;
+  }
 
   return getValue({ $ }, config);
 }
