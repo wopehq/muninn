b30bbb7209b4462ac8b3ad905719268dd6e867d2
Fatih Kaya <1994274@gmail.com>
2022-06-15 16:31:25 +0300

    basic schema extract test

--- src/parser/getSchemaValue.spec.ts
+++ src/parser/getSchemaValue.spec.ts
@@ -5,10 +5,28 @@ import getSchemaValue from './getSchemaValue';
 const sample = `
   <div id="root">
     <div class="text">sample</div>
+    <a href="https://example.com">example.com</a>
   </div>
 `;
 
 describe('getSchemaValue', () => {
+  it('ExtractBasicSchema', () => {
+    const schema: Schema = {
+      link: {
+        selector: 'a',
+        schema: { title: '', url: '@ href' }
+      }
+    };
+    const $ = load(sample);
+    const el = $('#root') as Cheerio<Element>;
+    const result = getSchemaValue({ $, el }, schema);
+    const expected = {
+      link: { title: 'example.com', url: 'https://example.com' }
+    };
+
+    expect(result).toStrictEqual(expected);
+  });
+
   it('SchemaValueConfigUnion', () => {
     const schema: Schema = {
       val: [
