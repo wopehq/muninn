327292511a0edf68a01362ad335d7dab8a3e309a
Murat Güngör <ecoderat@gmail.com>
2025-02-17 10:11:28 +0300

    refactor: simplify URL processing to fit general use cases

--- src/parser/methods/url.ts
+++ src/parser/methods/url.ts
@@ -1,27 +1,5 @@
 import execRegex from '../regex/execRegex';
 
-const paramsToRemove = new Set(['sa', 'sqi', 'ved', 'usg']);
-
-const url = (val) => {
-  if (val.startsWith('/search?')) {
-    return val;
-  }
-
-  let extractedUrl = execRegex(val, 'url');
-  if (!extractedUrl) return null;
-
-  return cleanUrl(extractedUrl);
-};
-
-const cleanUrl = (url) => removeTextFragment(removeParams(url));
-
-const removeTextFragment = (url) =>
-  url.replace(/(#|%23):~:text(=|%3D)[^#&]*/g, '');
-
-const removeParams = (url) =>
-  url
-    .split('&')
-    .filter((param) => !paramsToRemove.has(param.split('=')[0]))
-    .join('&');
+const url = (val: any) => execRegex(val, 'url');
 
 export default url;
